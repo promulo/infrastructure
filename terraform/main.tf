aaef75b54f93bee5aadd954b29afd3f6173fd10c
Paulo Barros <paulo.barros@crvsh.io>
2020-08-08 15:46:18 +0200

    Remove un-managed VPC from Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,13 +10,6 @@ data "template_file" "minion_user_data" {
   }
 }
 
-// FIXME: remove this resource from Terraform
-resource "digitalocean_vpc" "blog-vpc" {
-  name     = "blog-network"
-  region   = "fra1"
-  ip_range = "10.0.0.0/24"
-}
-
 resource "digitalocean_vpc" "default" {
   name     = "default-network"
   region   = "fra1"
