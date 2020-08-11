9993e44a80bff21ab046061f8b8770fac70a5ff0
Paulo Barros <paulo.barros@crvsh.io>
2020-08-11 23:29:26 +0200

    Declare variable for SSH key

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,3 +15,7 @@ variable "base_master_image_id" {
 variable "base_minion_image_id" {
     description = "Image ID for base minion"
 }
+
+variable "private_ssh_key_github" {
+    description = "Private SSH key for salt master to be able to clone infrastructure repo"
+}
